1b219cd6189a8a2a4ea95ec27785f9f318ed2aa3
Pereverzev Marat <marat.pereverzrv@gmail.com>
2024-08-29 15:02:45 +0300

    refactor: mongodb atlas access

--- src/app/models/rates.ts
+++ src/app/models/rates.ts
@@ -3,7 +3,7 @@ import type { IRate } from "@/utils/types";
 
 const RateSchema = new Schema<IRate>(
   {
-    id: String,
+    id: { type: String, required: true },
     symbol: { type: String, default: "null" },
     currencySymbol: { type: String, default: "null" },
     rateUsd: { type: String, default: "null" },
--- src/app/utils/mongo/db/index.ts
+++ src/app/utils/mongo/db/index.ts
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-const dbConnect = (): void => {
+const dbConnect = async (): Promise<void> => {
   try {
-    mongoose.connect(<string>process.env.MONGODB_URI);
-    console.log("Connected successfully");
+    await mongoose.connect(<string>process.env.MONGODB_URI);
+    console.log("connected to mongodb");
   } catch (err) {
     console.log(err);
   }
